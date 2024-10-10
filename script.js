de8c723616baab7ea57080c73e4d3d6b222f24a9
Henrique Fiorini Golfetto <henrique.fiorini@pedreira-local.org>
2024-10-10 16:45:43 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const item = document.getElementById("input-item");
+const botaoSalvarItem = document.getElementById("adicionar-item");
+
+botaoSalvarItem.addEventListener("click", adicionarItem);
+
+function adicionarItem(evento) {
+    evento.preventDefault()
+    console.log("entrei na função");
+}
